b29d68215d837cf127c77676684373b253098557
Kellen Bolger <bolger.kellen@gmail.com>
2018-09-08 19:07:40 -0400

    Initial Commit

---
+++ DoIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoIt
+//
+//  Created by Kellen Bolger on 2018-09-08.
+//  Copyright © 2018 Kellen Bolger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
